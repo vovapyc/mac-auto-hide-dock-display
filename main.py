89841d031cec23dd2880c2a899d5b80bab69d732
Restyled.io <commits@restyled.io>
2024-08-04 08:06:50 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -40,26 +40,41 @@ def get_display_info() -> list[bool, bool]:
     is_lid_closed = not any(display.is_builtin for display in all_displays)
 
     logger.debug(
-        f"External display connected: {is_external}, Lid closed: {is_lid_closed}")
+        f"External display connected: {is_external}, Lid closed: {is_lid_closed}"
+    )
     return is_external, is_lid_closed
 
 
 def hide_dock():
-    os.system("osascript -e 'tell application \"System Events\" to set the autohide of the dock preferences to true'")
+    os.system(
+        "osascript -e 'tell application \"System Events\" to set the autohide of the dock preferences to true'"
+    )
 
 
 def show_dock():
-    os.system("osascript -e 'tell application \"System Events\" to set the autohide of the dock preferences to false'")
+    os.system(
+        "osascript -e 'tell application \"System Events\" to set the autohide of the dock preferences to false'"
+    )
 
 
 def main():
     logger.info("Starting...")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interval", type=int,
-                        default=DEFAULT_SLEEP_TIME_SECONDS, help="Interval between checks")
-    parser.add_argument("-l", "--closed-lid", action="store_true",
-                        default=False, help="Show dock only when the lid is closed")
+    parser.add_argument(
+        "-i",
+        "--interval",
+        type=int,
+        default=DEFAULT_SLEEP_TIME_SECONDS,
+        help="Interval between checks",
+    )
+    parser.add_argument(
+        "-l",
+        "--closed-lid",
+        action="store_true",
+        default=False,
+        help="Show dock only when the lid is closed",
+    )
     args = parser.parse_args()
 
     last_state = None
@@ -67,8 +82,7 @@ def main():
         is_external, is_lid_closed = get_display_info()
 
         if (is_external, is_lid_closed) != last_state:
-            logger.info(
-                f"External display state changed to is_external={is_external}")
+            logger.info(f"External display state changed to is_external={is_external}")
             if is_external:
                 if args.closed_lid:
                     if is_lid_closed:
